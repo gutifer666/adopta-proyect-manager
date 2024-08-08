451864658c2ae83b24e33d49d25ea88808017d08
Guti <gutifer666@gmail.com>
2024-08-08 10:44:39 +0200

    primer commit

---
+++ src/main/java/trade/javiergutierrez/adopt_junior_project_manager/AdoptJuniorProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package trade.javiergutierrez.adopt_junior_project_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptJuniorProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptJuniorProjectManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trade/javiergutierrez/adopt_junior_project_manager/AdoptJuniorProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package trade.javiergutierrez.adopt_junior_project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptJuniorProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
